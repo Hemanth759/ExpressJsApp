a2911a9023df73ffb5bf1daca58ad90e8968c647
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-08 23:52:05 +0530

    Made few changes

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+// initailizing a app instance
 var app = express_1.default();
+// setting a port number
+var PORT = process.env.PORT || '5000';
 app.get('/', function (req, res, next) { res.send('Hello'); });
-app.listen(5000, function () { return console.log('Server running'); });
+app.listen(parseInt(PORT), function () { return console.log("Server running on PORT: " + PORT); });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import path from 'path';
 
+// initailizing a app instance
 const app: Application = express();
+
+// setting a port number
+const PORT: string = process.env.PORT || '5000';
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {res.send('Hello')});
 
-app.listen(5000, () => console.log('Server running'));
+app.listen(parseInt(PORT), () => console.log(`Server running on PORT: ${PORT}`));
