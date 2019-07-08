daccb3588d824edb3b7b6479bd38fc73f2efa6c0
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-09 00:08:39 +0530

    Added sendfile method to app.get

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ const app: Application = express();
 // setting a port number
 const PORT: string = process.env.PORT || '5000';
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {res.send('Hello')});
+app.get('/', (req: Request, res: Response, next: NextFunction) => { 
+    res.sendFile(path.join(__dirname, "..", "public", 'index.html'));
+});
 
 app.listen(parseInt(PORT), () => console.log(`Server running on PORT: ${PORT}`));
