d865e2776d92d736ae0f035e2e68bcf60de58f1f
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-09 13:46:58 +0530

    created objects for members and api for the member objects

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import path from 'path';
 
+import { Members } from './staticOjects/members';
+
 // initailizing a app instance
 const app: Application = express();
 
 // setting a port number
 const PORT: string = process.env.PORT || '5000';
 
+app.get('/api/members', (req: Request, res: Response) => {
+  res.json(Members);
+})
+
+// Set static folder
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(parseInt(PORT), () => console.log(`Server running on PORT: ${PORT}`));
---
+++ src/staticOjects/members.ts
@@ -0,0 +1,22 @@
+const members: Array<Object> = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Bob williams',
+    email: 'bob@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannonJackson@gmail.com',
+    status: 'active',
+  },
+];
+
+export { members as Members };
