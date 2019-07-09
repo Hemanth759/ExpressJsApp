34694d356655523768306dbff0dd0e5a2b38495f
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-09 14:35:02 +0530

    Added logger to different file

---
+++ src/middleware/logger.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import moment from 'moment';
+
+const logger = (req: Request, res: Response, next: NextFunction): void => {
+  console.log(`\n${req.protocol.toString()}://${req.get('host')}${req.url}`);
+  next();
+}
+
+export { logger as Logger}; 
