9bfc4e9c14d93a4fa4d69ad739837a85bd7c43cb
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-09 13:58:17 +0530

    Added middleware func to app

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,16 @@ import { Members } from './staticOjects/members';
 // initailizing a app instance
 const app: Application = express();
 
+const logger = (req: Request, res: Response, next: NextFunction): void => {
+  console.log(`\n${req.protocol.toString()}://${req.get('host')}${req.url}`);
+  next();
+}
+
 // setting a port number
 const PORT: string = process.env.PORT || '5000';
+
+// init Middlewre
+app.use(logger);
 
 app.get('/api/members', (req: Request, res: Response) => {
   res.json(Members);
