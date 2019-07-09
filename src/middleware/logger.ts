1caeab29a9fae12ce0f41f6d550f8af34ba24de0
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-07-09 14:36:19 +0530

    Added date display to the console in middleware

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import moment from 'moment';
 
 const logger = (req: Request, res: Response, next: NextFunction): void => {
-  console.log(`\n${req.protocol.toString()}://${req.get('host')}${req.url}`);
+  console.log(`\n${req.protocol.toString()}://${req.get('host')}${req.url}  ${moment().format()}`);
   next();
 }
 
